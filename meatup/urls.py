38dec42725565453d81e4e9c7230bbaa8b4e01f3
aernanto <exhamusgoldfeder@gmail.com>
2024-10-27 13:29:35 +0700

    Meatup dikit lagi v2

--- meatup/urls.py
+++ meatup/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-# from . import views
-from meatup.views import show_requests, received_requests, create_request, update_request_status, delete_request
+from meatup.views import (
+    show_requests, received_requests, create_request, update_request_status, delete_request,
+    wishlist_list, agree_request, decline_request, add_to_wishlist
+)
 
 app_name = 'meatup'
 
@@ -10,8 +12,8 @@ urlpatterns = [
     path('requests/new/<int:wishlist_id>/', create_request, name='create_request'),
     path('requests/update/<int:pk>/', update_request_status, name='update_request_status'),
     path('requests/delete/<int:pk>/', delete_request, name='delete_request'),
-    # path('main_wishlist/', wishlist_list, name='wishlist_list'),
-    # path('requests/<int:id>/agree/', agree_request, name='agree_request'),
-    # path('requests/<int:id>/decline/', decline_request, name='decline_request'),
-    # path('meatup/', include('meatup.urls', namespace='meatup')),
+    path('main_wishlist/', wishlist_list, name='wishlist_list'),
+    path('requests/<int:id>/agree/', agree_request, name='agree_request'),
+    path('requests/<int:id>/decline/', decline_request, name='decline_request'),
+    path('add_to_wishlist/', add_to_wishlist, name='add_to_wishlist'),  # New URL for AJAX add item
 ]
