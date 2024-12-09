715b86263f9d959e0d74bdaf4f762743fd1fd0ba
aernanto <exhamusgoldfeder@gmail.com>
2024-12-09 14:10:47 +0700

    Revised paths and views

--- meatup/urls.py
+++ meatup/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('create/', views.create_message_entry, name='create_message_entry'),
     path('edit/<int:id>/', views.edit_message, name='edit_message'),
     path('delete/<int:id>/', views.delete_message, name='delete_message'),
+    path('flutter/', views.meatup_home_flutter, name='meatup_home_flutter'),
+    path('flutter/create/', views.create_message_flutter, name='create_message_flutter'),
+    path('flutter/edit/<int:id>/', views.edit_message_flutter, name='edit_message_flutter'),
+    path('flutter/delete/<int:id>/', views.delete_message_flutter, name='delete_message_flutter'),
 ]
